84d18a95512ac24984a798bd3642eec0ad8f2c94
kumaranand2905 <kumar.anand@berylsystems.com>
2018-01-22 11:03:14 +0530

    Code Optimization

--- Student/app/controllers/api/v1/posts_controller.rb
+++ Student/app/controllers/api/v1/posts_controller.rb
@@ -1,13 +1,9 @@
 class Api::V1::PostsController < ApplicationController
 	skip_before_action :verify_authenticity_token 
 
-
 	def index
-		#debugger
-		#@posts = Post.all
-		@user = User.find(params[:user_id])
+	  @user = User.find(params[:user_id])
 		@posts =@user.posts.all
-
 		render json: {post: JSONAPI::Serializer.serialize(@posts,serializer: PostSerializer, is_collection: true),status: 200}
 	end
 
@@ -19,23 +15,17 @@ class Api::V1::PostsController < ApplicationController
 
 	def create
 		@user = User.find(params[:user_id])
-		#@post = @user.posts.create(post_params)
 		@post = @user.post.new(name: params[:title],email: params[:content])
 		if @user.save
       @user.update(user_params) 
       render json: @user
    else
      render json: { message: "User cannot be saved.", :status => 406}
-		render json: @post
+	   render json: @post
+	 end
 	end
-	end
 
-
-
-
-
-
-private
+	private
 
 	def post_params
 		params.permit(:title, :content)
